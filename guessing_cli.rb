36efb78e09d5cfd753c240ffa375e27f3376fed1
Adam Johnson <adamwjo@gmail.com>
2018-08-24 16:46:38 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,10 +3,11 @@ require "pry"
 
 def run_guessing_game
   user_input = ""
-  binding.pry
+
   random_num = rand(1..6).to_s
   puts "Guess a number between 1 and 6."
   user_input = gets.downcase.chomp
+  
   if user_input == random_num
     puts "You guessed the correct number!"
     return
